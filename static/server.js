35bd0ba013062725f6eaacebbc735db520cd05b8
zeeshan-sowedane <129268446+zeeshan-sowedane@users.noreply.github.com>
2023-03-29 15:40:37 +0530

    Add files via upload

---
+++ static/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString:
+    "postgresql://postgres:mysecretpassword@localhost:5432/message_boards",
+});
+
+async function init() {
+  const app = express();
+
+  app.get("/get", async (req, res) => {
+    const client = await pool.connect();
+    const [commentsRes, boardRes] = await Promise.all([
+      client.query(
+        `SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = ${req.query.search}`
+        // "SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = $1",
+        // [req.query.search]
+      ),
+      client.query("SELECT * FROM boards WHERE board_id = $1", [
+        req.query.search,
+      ]),
+    ]);
+    res
+      .json({
+        status: "ok",
+        board: boardRes.rows[0] || {},
+        posts: commentsRes.rows,
+      })
+      .end();
+    await client.end();
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.use(express.static("./static"));
+  app.listen(PORT);
+
+  console.log(`running on http://localhost:${PORT}`);
+}
+init();
